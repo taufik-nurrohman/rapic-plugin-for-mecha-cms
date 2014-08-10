d77d7c6490be649b137a73cbc920e267dfe20087
Taufik Nurrohman <t.nurrohman77@gmail.com>
2014-08-10 09:05:04 +0700

    For Mecha 1.1.0

--- rapic/configurator.php
+++ rapic/configurator.php
@@ -1,15 +1,15 @@
-<form class="form-plugin" action="<?php $_config = unserialize(File::open(PLUGIN . DS . 'rapic' . DS . 'states' . DS . 'config.txt')->read()); echo $config->url_current; ?>/update" method="post">
-  <input name="token" type="hidden" value="<?php echo Guardian::makeToken(); ?>">
+<form class="form-plugin" action="<?php $rapic_config = File::open(PLUGIN . DS . 'rapic' . DS . 'states' . DS . 'config.txt')->unserialize(); echo $config->url_current; ?>/update" method="post">
+  <input name="token" type="hidden" value="<?php echo $token; ?>">
   <label class="grid-group">
     <span class="grid span-1 form-label"><?php echo $speak->plugin_rapic_title_ad_code; ?></span>
     <span class="grid span-5">
-      <textarea name="ad_code" class="input-block code"><?php echo Text::parse($_config['ad_code'])->to_encoded_html; ?></textarea>
+      <textarea name="ad_code" class="input-block code"><?php echo Text::parse($rapic_config['ad_code'])->to_encoded_html; ?></textarea>
     </span>
   </label>
   <label class="grid-group">
     <span class="grid span-1 form-label"><?php echo $speak->plugin_rapic_title_ad_css; ?></span>
     <span class="grid span-5">
-      <textarea name="ad_css" class="input-block code"><?php echo Text::parse($_config['ad_css'])->to_encoded_html; ?></textarea>
+      <textarea name="ad_css" class="input-block code"><?php echo Text::parse($rapic_config['ad_css'])->to_encoded_html; ?></textarea>
     </span>
   </label>
   <div class="grid-group">
--- rapic/launch.php
+++ rapic/launch.php
@@ -2,12 +2,6 @@
 
 $ad_config = unserialize(File::open(PLUGIN . DS . 'rapic' . DS . 'states' . DS . 'config.txt')->read());
 
-if( ! $language = File::exist(PLUGIN . DS . 'rapic' . DS . 'languages' . DS . $config->language . DS . 'speak.txt')) {
-    $language = PLUGIN . DS . 'rapic' . DS . 'languages' . DS . 'en_US' . DS . 'speak.txt';
-}
-
-Config::merge('speak', Text::toArray(File::open($language)->read()));
-
 function random_ad_position_in_article_and_page_content($content) {
     global $ad_config;
     $paragraph = preg_split('#<\/p>#', $content, null, PREG_SPLIT_NO_EMPTY);
@@ -37,7 +31,7 @@ Route::accept($config->manager->slug . '/plugin/rapic/update', function() use($c
     if($request = Request::post()) {
         Guardian::checkToken($request['token']);
         unset($request['token']); // Remove token from request array
-        File::write(serialize($request))->saveTo(PLUGIN . DS . 'rapic' . DS . 'states' . DS . 'config.txt');
+        File::serialize($request)->saveTo(PLUGIN . DS . 'rapic' . DS . 'states' . DS . 'config.txt');
         Notify::success(Config::speak('notify_success_updated', array($speak->plugin)));
         Guardian::kick(dirname($config->url_current));
     }
