053e9dcc43d207a29981a0f4515a97b093168080
Taufik Nurrohman <t.nurrohman77@gmail.com>
2015-06-04 17:21:46 +0700

    Disable Ad Insertion Through Custom Fields

--- rapic/launch.php
+++ rapic/launch.php
@@ -4,7 +4,7 @@
 $rapic_config = File::open(PLUGIN . DS . basename(__DIR__) . DS . 'states' . DS . 'config.txt')->unserialize();
 
 // The random ad position function
-function do_random_ad_position_in_article_and_page_content($content) {
+function do_random_ad_position($content) {
     global $rapic_config;
     $paragraph = explode('</p>', $content);
     array_splice($paragraph, array_rand($paragraph), 0, $rapic_config['ad_code'] . '<rapic:end>');
@@ -12,11 +12,18 @@ function do_random_ad_position_in_article_and_page_content($content) {
     return str_replace(array('<rapic:end></p>', '<rapic:end>'), "", $paragraph);
 }
 
-// Register the filters
-Filter::add('article:content', 'do_random_ad_position_in_article_and_page_content');
-Filter::add('page:content', 'do_random_ad_position_in_article_and_page_content');
-
-// Include the CSS
-Weapon::add('shell_after', function() use($config, $rapic_config) {
-    echo O_BEGIN . '<style>' . ($config->html_minifier ? Converter::detractShell($rapic_config['ad_css']) : NL . $rapic_config['ad_css'] . NL) . '</style>' . O_END;
-});
+if($config->page_type === 'article' || $config->page_type === 'page') {
+    // Register the filters
+    Filter::add('shield:lot', function($data) use($config) {
+        if( ! isset($data[$config->page_type]->fields->disable_rapic)) {
+            if(isset($data[$config->page_type]->content)) {
+                $data[$config->page_type]->content = do_random_ad_position($data[$config->page_type]->content);
+            }
+        }
+        return $data;
+    });
+    // Include the CSS
+    Weapon::add('shell_after', function() use($config, $rapic_config) {
+        echo O_BEGIN . '<style>' . ($config->html_minifier ? Converter::detractShell($rapic_config['ad_css']) : NL . $rapic_config['ad_css'] . NL) . '</style>' . O_END;
+    });
+}
---
+++ rapic/workers/fields.php
@@ -0,0 +1,8 @@
+<?php
+
+return array(
+    'disable_rapic' => array(
+        'title' => $speak->plugin_rapic_title_ad_disable,
+        'type' => 'b'
+    )
+);
